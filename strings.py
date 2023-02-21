dbe6f270a46eeabfce95398a930cd0cdb0d07201
kaleab <kalu.cych@gmail.com>
2023-02-21 00:37:04 -0800

    basics about strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+# Strings are usually stored in brackets.
+
+some_str = "This is a string"
+print(type(some_str)) # --- output <class 'str'>
+
+
+# Long strings are declared using three single quotes
+# Example:
+
+very_long = '''
+-------
+-----
+----
+-
+----
+-----
+------
+-------
+'''
+
+print(very_long)
