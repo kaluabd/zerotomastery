06c1a30048a88ef99bfb904fdd243d62d1b24ecc
kaleab <kalu.cych@gmail.com>
2023-02-20 23:47:41 -0800

    complex number and conveting to binary added

---
+++ complex_datatype.py
@@ -0,0 +1,15 @@
+#complex data type is pretty complex
+#I may never use it, but helps to know
+#it mostly used in complex math
+
+
+#Binary representation of numbers
+binary = 10
+converted = bin(binary)
+print(converted)
+
+decimal = int(converted, 2)
+
+print(decimal)
+
+#Revesring the binary to decimal
