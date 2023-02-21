8928947a93b276424f76eb20d72ea5dc4874e63a
kaleab <kalu.cych@gmail.com>
2023-02-21 00:13:14 -0800

    Details about variables is added

---
+++ variables.py
@@ -0,0 +1,24 @@
+# variables store information that is used in our program
+# some properties and best use of variables
+
+#   snake_case  ---- makes it easy to read
+#   Starts with lowercase or underscore
+#   Letters, numbers, and underscores could be included in variable names
+#   They are case sensetive
+#   We dont wanna write key words ---- meaning we can not assign a variable name that already mean something in python (like print)
+#   Name them what they meant ---- meaning make for other readers easy to understand
+
+# When we want to store constantnts, we may use a convention of using capital letters to tell other developers('this variable is a constant')
+# example:
+
+PI = (3.14)
+print(PI) 
+
+
+# let just say we are storing our height in a variable and printing it
+
+my_height = 185
+print(my_height)
+
+
+# dunder variables are those which start with double underscore(__) ---- they are to be left alone
