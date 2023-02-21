674a62e466951dc425b9902c4e6879af4ead2d72
kaleab <kalu.cych@gmail.com>
2023-02-21 03:46:48 -0800

    immutability is added

---
+++ immutability.py
@@ -0,0 +1,10 @@
+# Strings in python are immutable
+# Example
+
+just_string = '01234'
+just_string[0] = '1'
+print(just_string[0])
+
+
+# The above code throws a TypeError saying string object does not support item assignment
+# Because of this strings in python are immutable
