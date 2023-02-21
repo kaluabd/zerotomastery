20160063abddd433ac8568d7c6677e13b0a6de69
kaleab <kalu.cych@gmail.com>
2023-02-21 00:20:46 -0800

    expression vs statement is added

---
+++ expression_vs_statement.py
@@ -0,0 +1,6 @@
+# expression is what gives us a value. 
+# in the following example (age+50) is an expression
+# a statement is the full line of code (user_salary = age + 50)
+age = 30
+user_salary = age + 50
+
