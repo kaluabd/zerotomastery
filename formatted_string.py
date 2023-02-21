157117ca9dfea1d0a64fcca7c94bf667b2e95617
kaleab <kalu.cych@gmail.com>
2023-02-21 01:12:19 -0800

    formatted strings

---
+++ formatted_string.py
@@ -0,0 +1,13 @@
+# Formatted string --- print(f'your string {variable name}.')
+# Is used to dyanmically change in 
+
+name = "john"
+
+# The old way
+print('hello '+ name)
+
+# The formatted string old way in python to form (using .format)
+print('hello {}, are you {}?'.format('kyle','fine'))
+
+# The python3 way --- nice and clear --- so much easier
+print(f'hello {name}')
