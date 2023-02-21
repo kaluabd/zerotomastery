a0eca929ab6673784637100cd051f30e504f3dc1
kaleab <kalu.cych@gmail.com>
2023-02-21 00:30:01 -0800

    augmented assignment

---
+++ augmentd_assignment_operator.py
@@ -0,0 +1,14 @@
+# code without augmented assignment operator
+
+salary = 10000
+bonus = 2000
+
+total_income = salary + bonus
+print(total_income)
+
+# code with augmented assignment operator
+
+new_salary = 10000
+new_salary += 2000 # using += uses to add the value we specify to the already assigned variable
+
+print(new_salary)
