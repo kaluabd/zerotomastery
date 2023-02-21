698a648cf552533f93f23d7ebc3fbed1b6269b93
kaleab <kalu.cych@gmail.com>
2023-02-21 00:53:52 -0800

    escape sequence in detail

---
+++ escape_sequence.py
@@ -0,0 +1,16 @@
+# Escape Sequence
+# Some of escape sequence include: \, \n, \t
+# \t is used to tab
+# \n is used to make new line
+# What if we need to use single quotes and double quotes in our statements?
+
+# The \ is used to escape sequence in order to tell python whatever comes after this is a stirng
+taste = 'It\'s sweet' # the escape sequence character saves the day
+
+# Another Example
+print("Today's weather is very \"hot\"") 
+
+# what if we wanted to print the back slash?
+print("this is a back slash \\") # python takes the first slash to know the next character is a string
+
+
