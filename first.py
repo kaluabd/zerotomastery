eb29d4b5ffd4cdf325669dfb1a560d00dc3e5300
kaleab <kalu.cych@gmail.com>
2023-02-20 23:37:12 -0800

    precedence is done

--- first.py
+++ first.py
@@ -1,3 +1,8 @@
 #operator precedence
-
 #the set of rules that python follows to do maths
+#In order of sequece precedence comes as follows
+
+# () ---- comes first
+# ** ---- comes second
+# */ ---- comes third
+# +- ---- comes forth
