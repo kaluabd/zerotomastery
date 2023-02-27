a570ce5c26347dc92e42de6853cc2b9c39f059b8
kaleab <kalu.cych@gmail.com>
2023-02-26 23:42:17 -0800

    description and example of a list is added

---
+++ list.py
@@ -0,0 +1,11 @@
+# List is an ordered sequence of objects
+# List is a form of array, however they have their differences.
+
+
+# Data structure is like a cart, which can store different types of objects.
+# Example:
+
+cart = ['phones','laptops', 3, 3.7]
+print(cart[:3])
+print(cart[0])
+print(type(cart))
