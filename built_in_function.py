4a40b56e32e0428510fd3707b3f0a6ebe798241a
kaleab <kalu.cych@gmail.com>
2023-02-26 23:12:21 -0800

    Git error fixed

---
+++ built_in_function.py
@@ -0,0 +1,5 @@
+# build in functions are functions like:
+
+# print(), len(), there are not a lot of them, 
+
+# Google for future reference
--- immutability.py
+++ immutability.py
@@ -8,3 +8,4 @@ print(just_string[0])
 
 # The above code throws a TypeError saying string object does not support item assignment
 # Because of this strings in python are immutable
+# The only way to change the value is to create something new 
