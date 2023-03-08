903b2c6dc68a1059da2facf90c9bcac60f46d4de
kaleab <kalu.cych@gmail.com>
2023-03-07 22:49:51 -0800

    new commit to tuples

--- tuple.py
+++ tuple.py
@@ -3,5 +3,20 @@
 # It is like a list but can not be changed
 # Used to store something that stays without changing.
 
-my_tuple = (1,2,3,4,5,6)
+my_tuple = (1,2,3,4,4,4,5,6)
 print(my_tuple)
+
+# Another example
+print(my_tuple.count(4)) # counts how many times num 4 is repeated
+print(my_tuple.index(3))
+
+# example of dictionary
+# Dictinary has a key:value positioning.
+user = {
+    'name': 'kyle',
+    'age': 25,
+    'location': 'London'
+}
+
+print(user)
+print(user.items())
