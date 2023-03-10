309f39ffef9dde31bd2a37c1219484eda8e4345d
kaleab <kalu.cych@gmail.com>
2023-03-09 23:28:40 -0800

    simple exercise on logic

--- short_circuting.py
+++ short_circuting.py
@@ -1,7 +1,17 @@
 # Short Circuiting
+# It is like circuit logic that process the output of the logical value.
 
 is_Red = True
 is_sweet = False
 
 print(is_Red and is_sweet)
 print(is_Red or is_sweet )
+
+
+# Another example on logical operator
+# >, <, ==, >=, <=
+
+print(ord('a'))  # this shows the unicode code point.
+print(ord('y'))
+
+print('a'>'y')
